9147dee6ff4de47462c41c4725fc6ca074034e6c
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-08-10 18:27:16 +0700

    Fix broken Link(s) Visible on Index Page(s)

--- markdown-smart-internal-link/index.php
+++ markdown-smart-internal-link/index.php
@@ -7,36 +7,40 @@ function fn_markdown_replace_link($content, $lot) {
     if (strpos($content, '[link:') === false) {
         return $content;
     }
-    global $language, $url;
+    global $language, $site, $url;
     $links = "";
-    return preg_replace_callback('#(?:\[(.*?)\])?\[link:((?:\.{2}/)*)([a-z\d/-]*?)([?&\#].*?)?\]#', function($m) use(&$links, $language, $url) {
-        $pp = Path::D($url->path);
+    return preg_replace_callback('#(?:\[(.*?)\])?\[link:((?:\.{2}/)*)([a-z\d/-]*?)([?&\#].*?)?\]#', function($m) use(&$links, $language, $site, $url) {
+        $u = $url->path;
+        if ($site->is === 'page') {
+            $u = Path::D($u);
+        }
         if (!empty($m[2]) && ($i = substr_count($m[2], '../')) !== 0) {
-            $pp = Path::D($pp, $i);
+            $u = Path::D($u, $i);
             $m[2] = str_replace('../', "", $m[2]);
         }
-        $pp = $pp ? '/' . $pp : "";
+        $u = $u ? '/' . $u : "";
         if (empty($m[2]) && strpos($m[3], '/') === 0) {
             $p = PAGE . $m[3];
         } else {
-            $p = PAGE . $pp . '/' . $m[3];
+            $p = PAGE . $u . '/' . $m[3];
         }
         $m[4] = isset($m[4]) ? $m[4] : "";
         $p = To::path($p);
-        $ff = File::exist([$p . '.page', $p . '.archive']);
-        if ($m[3] && !$ff) {
+        $f = File::exist([$p . '.page', $p . '.archive']);
+        if ($m[3] && !$f) {
             return HTML::s($m[1] ?: $language->link_broken, [
                 'title' => $m[1] ? $language->link_broken : $m[0],
                 'css' => ['color' => '#f00']
             ]);
         }
         $t = To::title(Path::B($m[2]));
-        $title = Page::open($ff)->get('title', $t);
-        $title_text = Page::open($ff, [], "")->get('title', $t);
-        $uid = md5($m[3] . $m[4]) . '-' . uniqid(); // Unique ID
-        $links .= "\n" . '[link:' . $uid . ']: ' . ($m[3] ? $url . To::url(strpos($m[3], '/') === 0 ? $m[3] : $pp . '/' . $m[3]) . $m[4] . ' "' . To::text($title) . '"' : $m[4]);
-        return '[' . ($m[1] ?: $title_text) . '][link:' . $uid . ']';
+        $title = Page::open($f)->get('title', $t);
+        $title_text = Page::apart(file_get_contents($f));
+        $title_text = isset($title_text['title']) ? $title_text['title'] : $t;
+        $id = md5($m[3] . $m[4]) . '-' . uniqid(); // Unique ID
+        $links .= "\n" . '[link:' . $id . ']: ' . ($m[3] ? $url . To::url(strpos($m[3], '/') === 0 ? $m[3] : $u . '/' . $m[3]) . $m[4] . ' "' . To::text($title) . '"' : $m[4]);
+        return '[' . ($m[1] ?: $title_text) . '][link:' . $id . ']';
     }, $content) . "\n" . $links;
 }
 
-Hook::set('page.content', 'fn_markdown_replace_link', 1.9);
+Hook::set(['page.content', 'page.description'], 'fn_markdown_replace_link', 1.9);
