3bb3d359641c2415c23b526fe54412d1edc23aca
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-08-16 10:54:06 +0700

    Small Bug Fix
    + Add hook to check broken links.

--- markdown-smart-internal-link/index.php
+++ markdown-smart-internal-link/index.php
@@ -10,21 +10,26 @@ function fn_markdown_replace_link($content, $lot) {
     global $language, $url;
     $links = "";
     return preg_replace_callback('#(?:\[(.*?)\])?\[link:((?:\.{2}/)*)([a-z\d/-]*?)([?&\#].*?)?\]#', function($m) use(&$links, $language, $lot, $url) {
-        $u = str_replace([PAGE . DS, DS], ["", '/'], dirname($lot['path']));
+        $u = rtrim(str_replace(PAGE . DS, "", dirname($lot['path']) . DS), DS);
         if (!empty($m[2]) && ($i = substr_count($m[2], '../')) !== 0) {
             $u = Path::D($u, $i);
             $m[2] = str_replace('../', "", $m[2]);
         }
-        $u = $u ? '/' . $u : "";
+        if ($u !== "") {
+            $u = '/' . $u;
+        }
         if (empty($m[2]) && strpos($m[3], '/') === 0) {
             $p = PAGE . $m[3];
         } else {
-            $p = PAGE . $u . '/' . $m[3];
+            $p = PAGE . $u . DS . $m[3];
         }
         $m[4] = isset($m[4]) ? $m[4] : "";
-        $p = To::path($p);
-        $f = File::exist([$p . '.page', $p . '.archive']);
+        $f = File::exist([
+            $p . '.page',
+            $p . '.archive'
+        ]);
         if ($m[3] && !$f) {
+            Hook::fire('on.' . basename(__DIR__) . '.x', [$m]);
             return HTML::s($m[1] ?: $language->link_broken, [
                 'title' => $m[1] ? $language->link_broken : $m[0],
                 'css' => ['color' => '#f00']
@@ -32,8 +37,7 @@ function fn_markdown_replace_link($content, $lot) {
         }
         $t = To::title(Path::B($m[2]));
         $title = Page::open($f)->get('title', $t);
-        $title_text = Page::apart(file_get_contents($f));
-        $title_text = isset($title_text['title']) ? $title_text['title'] : $t;
+        $title_text = Page::apart(file_get_contents($f), 'title', $t);
         $id = md5($m[3] . $m[4]) . '-' . uniqid(); // Unique ID
         $links .= "\n" . '[link:' . $id . ']: ' . ($m[3] ? $url . To::url(strpos($m[3], '/') === 0 ? $m[3] : $u . '/' . $m[3]) . $m[4] . ' "' . To::text($title) . '"' : $m[4]);
         return '[' . ($m[1] ?: $title_text) . '][link:' . $id . ']';
