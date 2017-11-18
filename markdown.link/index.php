4973448270efb50bc0d186a4426e67803f5a893c
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-11-18 21:30:16 +0700

    Small Bug Fix
    [link:] → maps to parent page
    [link:..] → maps to parent parent’s page

--- markdown.link/index.php
+++ markdown.link/index.php
@@ -9,11 +9,16 @@ function fn_markdown_link($content, $lot) {
     }
     global $language, $url;
     $links = "";
-    return preg_replace_callback('#(?:\[(.*?)\])?\[link:((?:\.{2}/)*)([a-z\d/-]*?)([?&\#].*?)?\]#', function($m) use(&$links, $language, $lot, $url) {
+    return preg_replace_callback('#(?:\[(.*?)\])?\[link:((?:\.{2}/)*|\.{2})([a-z\d/-]*?)([?&\#].*?)?\]#', function($m) use(&$links, $language, $lot, $url) {
         $u = rtrim(str_replace(PAGE . DS, "", dirname($lot['path']) . DS), DS);
-        if (!empty($m[2]) && ($i = substr_count($m[2], '../')) !== 0) {
-            $u = Path::D($u, $i);
-            $m[2] = str_replace('../', "", $m[2]);
+        if (!empty($m[2])) {
+            if ($m[2] === '..' && empty($m[3])) {
+                $u = Path::D($u);
+                $m[2] = "";
+            } else if (($i = substr_count($m[2], '../')) !== 0) {
+                $u = Path::D($u, $i);
+                $m[2] = str_replace('../', "", $m[2]);
+            }
         }
         if ($u !== "") {
             $u = '/' . $u;
@@ -32,14 +37,14 @@ function fn_markdown_link($content, $lot) {
             Hook::fire('on.' . basename(__DIR__) . '.x', [$url->current, $lot, $m]);
             return HTML::s($m[1] ?: $language->link_broken, [
                 'title' => $m[1] ? $language->link_broken : $m[0],
-                'css' => ['color' => '#f00']
+                'style[]' => ['color' => '#f00']
             ]);
         }
         $t = To::title(Path::B($m[2]));
         $title = Page::open($f)->get('title', $t);
         $title_text = is_file($f) ? Page::apart(file_get_contents($f), 'title', $t) : $t;
         $id = md5($m[3] . $m[4]) . '-' . uniqid(); // Unique ID
-        $links .= "\n" . '[link:' . $id . ']: ' . ($m[3] ? $url . To::url(strpos($m[3], '/') === 0 ? $m[3] : $u . '/' . $m[3]) . $m[4] . ' "' . To::text($title) . '"' : $m[4]);
+        $links .= "\n" . '[link:' . $id . ']: ' . ($m[3] ? $url . To::url(strpos($m[3], '/') === 0 ? $m[3] : $u . '/' . $m[3]) . $m[4] . ' "' . To::text($title) . '"' : $url . To::url($u) . $m[4]);
         return '[' . ($m[1] ?: $title_text) . '][link:' . $id . ']';
     }, $content) . "\n" . $links;
 }
