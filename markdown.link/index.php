b9e3395742418618bc12b35f9aa215b213928d08
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-10-12 12:07:30 +0700

    PHP 7.1.0 and Above
    http://mecha-cms.com/article/p-h-p-7-1-0

--- markdown.link/index.php
+++ markdown.link/index.php
@@ -1,25 +1,24 @@
-<?php
+<?php namespace fn\markdown;
 
-function fn_markdown_link($content, $lot = [], $that) {
-    if ($that->get('type') !== 'Markdown') {
+function link($content = "", array $lot = []) {
+    if ($this->type !== 'Markdown') {
         return $content;
     }
     if (strpos($content, '[link:') === false) {
         return $content;
     }
     global $language, $url;
-    $links = "";
-    return preg_replace_callback('#(?:\[(.*?)\])?\[link:((?:\.{2}/)*|\.{2})([\w/-]*?)([?&\#].*?)?\]#', function($m) use(&$links, $language, $lot, $url) {
-        if (!isset($lot['path'])) {
+    return preg_replace_callback('#(?:\[(.*?)\])?\[link:((?:\.{2}/)*|\.{2})([\w/-]*?)([?&\#].*?)?\]#', function($m) use($language, $lot, $url) {
+        if (!$path = $this->path) {
             return $m[0];
         }
-        $u = rtrim(str_replace(PAGE . DS, "", dirname($lot['path']) . DS), DS);
+        $u = rtrim(str_replace(PAGE . DS, "", dirname($path) . DS), DS);
         if (!empty($m[2])) {
             if ($m[2] === '..' && empty($m[3])) {
-                $u = Path::D($u);
+                $u = \Path::D($u);
                 $m[2] = "";
             } else if (($i = substr_count($m[2], '../')) !== 0) {
-                $u = Path::D($u, $i);
+                $u = \Path::D($u, $i);
                 $m[2] = str_replace('../', "", $m[2]);
             }
         }
@@ -32,24 +31,24 @@ function fn_markdown_link($content, $lot = [], $that) {
             $p = PAGE . $u . DS . $m[3];
         }
         $m[4] = isset($m[4]) ? $m[4] : "";
-        $f = File::exist([
+        $f = \File::exist([
             $p . '.page',
             $p . '.archive'
         ], null);
         if ($m[3] && !$f) {
-            Hook::fire('on.' . basename(__DIR__) . '.x', [$url->current, $lot, $m]);
-            return HTML::s($m[1] ?: $language->link_broken, [
+            \Hook::fire('on.' . basename(__DIR__) . '.x', [$m, $lot], $this);
+            return \HTML::s($m[1] ?: $language->link_broken, [
                 'title' => $m[1] ? $language->link_broken : $m[0],
                 'style[]' => ['color' => '#f00']
             ]);
         }
-        $t = To::title(Path::B($m[2]));
-        $title = Page::open($f)->get('title', $t);
-        $title_text = is_file($f) ? Page::apart(file_get_contents($f), 'title', $t) : $t;
+        $t = \To::title(\Path::B($m[2]));
+        $title = \Page::open($f)->get('title', $t);
+        $title_text = is_file($f) ? \Page::apart($f, 'title', $t) : $t;
         $id = md5($m[3] . $m[4]) . '-' . uniqid(); // Unique ID
-        $links .= "\n" . '[link:' . $id . ']: ' . ($m[3] ? $url . To::URL(strpos($m[3], '/') === 0 ? $m[3] : $u . '/' . $m[3]) . $m[4] . ' "' . To::text($title) . '"' : $url . To::URL($u) . $m[4]);
-        return '[' . ($m[1] ?: $title_text) . '][link:' . $id . ']';
-    }, $content) . "\n" . $links;
+        $u = $m[3] ? $url . \To::URL(strpos($m[3], '/') === 0 ? $m[3] : $u . '/' . $m[3]) . $m[4] . ' "' . \To::text($title) . '"' : $url . \To::URL($u) . $m[4];
+        return '[' . ($m[1] ?: $title_text) . '](' . $u . ')';
+    }, $content);
 }
 
-Hook::set(['*.content', '*.description'], 'fn_markdown_link', 1.9);
+\Hook::set(['*.content', '*.description'], __NAMESPACE__ . '\link', 1.9);
