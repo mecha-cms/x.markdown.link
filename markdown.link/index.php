8bb5da3b10594945f1b8403b8653a4e44918b393
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-07-17 10:25:21 +0700

    Markdown Link Plugin is Now Markdown Link Extension

--- markdown.link/index.php
+++ markdown.link/index.php
@@ -1,4 +1,4 @@
-<?php namespace _\markdown;
+<?php namespace _\lot\x\markdown;
 
 function link($content = "", array $lot = []) {
     if ($this['type'] !== 'Markdown') {
@@ -8,20 +8,21 @@ function link($content = "", array $lot = []) {
         return $content;
     }
     global $language, $url;
-    return \preg_replace_callback('#(?:\[([^]]*)\])?\[link:((?:\.{2}/)*|\.{2})([^\s?&\#]*)([?&\#].*)?\]#', function($m) use($language, $lot, $url) {
+    return \preg_replace_callback('/(?:\[([^]]*)\])?\[link:((?:\.{2}\/)*|\.{2})([^\s?&#]*)([?&#].*)?\]/', function($m) use($language, $lot, $url) {
         if (!$path = $this->path) {
             return $m[0];
         }
         $u = \rtrim(\str_replace(PAGE . DS, "", \dirname($path) . DS), DS);
         if (!empty($m[2])) {
             if ($m[2] === '..' && empty($m[3])) {
-                $u = \Path::D($u);
+                $u = \dirname($u);
                 $m[2] = "";
             } else if (0 !== ($i = \substr_count($m[2], '../'))) {
-                $u = \Path::D($u, $i);
+                $u = \dirname($u, $i);
                 $m[2] = \str_replace('../', "", $m[2]);
             }
         }
+        $u = $u === '.' ? "" : $u;
         if ($u !== "") {
             $u = '/' . $u;
         }
@@ -34,19 +35,23 @@ function link($content = "", array $lot = []) {
         $f = \File::exist([
             $p . '.page',
             $p . '.archive'
-        ], null);
+        ]);
         if ($m[3] && !$f) {
-            \Hook::fire('on.' . \basename(__DIR__) . '.x', [$m, $lot], $this);
-            return '<s title="' . ($m[1] ? $language->linkBroken : $m[0]) . '" style="color:#f00;">' . ($m[1] ?: $language->linkBroken) . '</s>';
+            \Hook::fire('on.markdown.link.x', [$m, $lot], $this);
+            return '<s title="' . ($m[1] ? $language->linkIsBroken : $m[0]) . '" style="color:#f00;">' . ($m[1] ?: $language->linkIsBroken) . '</s>';
         }
-        $t = \To::title((string) \Path::B($m[2]));
+        $t = \To::title(\basename($m[2]));
         $p = new \Page($f);
-        $title = $p->get('title', $t);
+        $title = $p->title ?? $t;
         $id = \md5($m[3] . $m[4]) . '-' . \uniqid(); // Unique ID
         $u = \strtr($m[3] ? $url . (\strpos($m[3], '/') === 0 ? $m[3] : $u . '/' . $m[3]) . $m[4] . ' "' . \To::text($title) . '"' : $url . $u . $m[4], DS, '/');
         return '[' . ($m[1] ?: $title) . '](' . $u . ')';
     }, $content);
 }
 
-\Hook::set(['*.content', '*.description'], __NAMESPACE__ . "\\link", 1.9); // Make sure to run before `_\markdown` hook
-\Language::set('link-broken', 'broken link');
+\Hook::set([
+    'page.content',
+    'page.description'
+], __NAMESPACE__ . "\\link", 1.9); // Make sure to run before `_\lot\x\markdown` hook
+
+\Language::set('link-is-broken', 'broken link');
